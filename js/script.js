50140572a5b431295688ba04fd2a95c2485472dd
Mayesha-Tasnim <mayesatasnim68@gmail.com>
2022-12-18 13:33:36 +0600

    responsive changes have been made

--- js/script.js
+++ js/script.js
@@ -122,3 +122,26 @@ function showSlides() {
 
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+//mobile slider
+
+let slideIndex2 = 0;
+
+showSlides2();
+function showSlides2() {
+  var j;
+
+  let slides2 = document.getElementsByClassName("images");
+
+  for (j = 0; j < slides2.length; j++) {
+    slides2[j].style.display = "none";
+  }
+  slideIndex2++;
+  if (slideIndex2 > slides2.length) {
+    slideIndex2 = 1;
+  }
+
+  slides2[slideIndex2 - 1].style.display = "block";
+
+  setTimeout(showSlides2, 2000); // Change image every 2 seconds
+}
